9684368b918d94ba9b4143a61a08f7ec958d6e93
Arvind Padmanabhan <arvindpdmn@gmail.com>
2026-06-24 12:52:41 +0530

    Maze updated to be more readable.

--- maze/maze.c
+++ maze/maze.c
@@ -38,26 +38,30 @@ The following are the objectives:
 /*--------------------------------------
 Type definitions
 ----------------------------------------*/
-typedef enum {
+typedef enum direction {
     North = 1,
     East = 2,
     South = 4,
     West = 8
 } Direction;
 
-typedef struct {
+typedef struct cell {
     int x;
     int y;
 } Cell;
 
+typedef struct maze {
+    int num_rows;
+    int num_cols;
+    char **lines; // as stored in file
+    char *m; // as encoded in memory
+} Maze;
 
-/*--------------------------------------
-Globals
-----------------------------------------*/
-int num_rows, num_cols;
-int found_path = 0;
-int num_steps; // indexes into path[]
-Cell path[100] = {};
+typedef struct path {
+    bool found_path;
+    int num_steps; // indexes into path[]
+    Cell *cells;
+} Path;
 
 
 /*--------------------------------------
@@ -77,90 +81,109 @@ int get_num_cols(FILE *fp) {
     return num_corners - 1;
 }
 
-char (*read_maze(char *filename, int *num_rows, int *num_cols))[] {
+int get_num_rows(FILE *fp) {
+    int num_lines = 0;
+
+    char line[100]; // an arbitrary limit
+    while (fgets(line, 100, fp) != NULL) {
+        num_lines++;
+    }
+
+    // Reset fp to start
+    fseek(fp, 0, SEEK_SET);
+
+    return (num_lines - 1) / 2;
+}
+
+void read_maze_from_file(Maze *maze, char *filename) {
     FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
-        printf("Error: unable to open maze file.\n");
-        return NULL;
+        printf("Error: unable to open maze file %s. Quitting...\n", filename);
+        exit(1);
     }
 
-    *num_cols = get_num_cols(fp);
-    *num_rows = *num_cols; // assume square maze
+    maze->num_cols = get_num_cols(fp);
+    maze->num_rows = get_num_rows(fp);
 
-    // Allocate maze memory and copy first row
-    char (*maze)[*num_cols] = calloc(*num_rows, sizeof(*maze));
+    int total_lines = 2 * (maze->num_rows) + 1;
+    int max_line_len = 4 * (maze->num_cols) + 3; // 4 chars per cell + 1 for boundary + '\n' + '\0'
 
-    // Read the entire maze
-    int total_lines = 2 * (*num_rows) + 1;
-    int max_line_len = 4 * (*num_cols) + 3; // 4 chars per cell + 1 for boundary + '\n' + '\0'
-
-    char **lines = malloc(total_lines * sizeof(char *));
+    maze->lines = malloc(total_lines * sizeof(char *));
     for (int i = 0; i < total_lines; i++) {
-        lines[i] = malloc((max_line_len) * sizeof(char));
-        if (fgets(lines[i], max_line_len, fp) == NULL) {
-            lines[i][0] = '\0';
+        maze->lines[i] = malloc((max_line_len) * sizeof(char));
+        if (fgets(maze->lines[i], max_line_len, fp) == NULL) {
+            maze->lines[i][0] = '\0';
         }
     }
-    fclose(fp);
 
-    for (int r = 0; r < *num_rows; r++) {
-        for (int c = 0; c < *num_cols; c++) {
+    fclose(fp);
+}
+
+void encode_maze(Maze *maze) {
+    maze->m = calloc(maze->num_rows * maze->num_cols, sizeof(char));
+    char (*m)[maze->num_cols] = (char (*)[maze->num_cols])maze->m;
+
+    for (int r = 0; r < maze->num_rows; r++) {
+        for (int c = 0; c < maze->num_cols; c++) {
             char cell_val = 0;
 
-            if (lines[2 * r][4 * c + 1] == '-') {
+            if (maze->lines[2 * r][4 * c + 1] == '-') {
                 cell_val |= North;
             }
-            if (lines[2 * r + 1][4 * c + 4] == '|') {
+            if (maze->lines[2 * r + 1][4 * c + 4] == '|') {
                 cell_val |= East;
             }
-            if (lines[2 * r + 2][4 * c + 1] == '-') {
+            if (maze->lines[2 * r + 2][4 * c + 1] == '-') {
                 cell_val |= South;
             }
-            if (lines[2 * r + 1][4 * c] == '|') {
+            if (maze->lines[2 * r + 1][4 * c] == '|') {
                 cell_val |= West;
             }
 
-            maze[r][c] = cell_val;
+            m[r][c] = cell_val;
         }
     }
-
-    for (int i = 0; i < total_lines; i++) {
-        free(lines[i]);
-    }
-    free(lines);
-
-    return maze;
 }
 
-void print_encoded_maze(int num_rows, int num_cols, char (*maze)[num_cols]) {
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 0; j < num_cols; j++) {
-            printf("0x%02X", (unsigned char)maze[i][j]);
-            if (j == num_cols-1) printf("\n");
+void free_maze(Maze *maze) {
+    int total_lines = 2 * (maze->num_rows) + 1;
+    for (int i = 0; i < total_lines; i++) {
+        free(maze->lines[i]);
+    }
+    free(maze->lines);
+
+    free(maze->m);
+}
+
+void print_encoded_maze(Maze *maze) {
+    for (int i = 0; i < maze->num_rows; i++) {
+        for (int j = 0; j < maze->num_cols; j++) {
+            printf("0x%02X", (unsigned char)maze->m[i*maze->num_cols + j]);
+            if (j == maze->num_cols-1) printf("\n");
             else printf(" ");
         }
     }
 }
 
-bool is_on_path(int r, int c, Cell *path, int num_steps) {
+bool is_on_path(int r, int c, Path *path) {
     if (path == NULL) return false;
-    for (int i = 0; i < num_steps; i++) {
-        // Assuming x is column index and y is row index (or vice versa depending on your implementation)
-        // Adjust if your struct defines x as row and y as column
-        if (path[i].x == r && path[i].y == c) {
+    for (int i = 0; i < path->num_steps; i++) {
+        if (path->cells[i].x == r && path->cells[i].y == c) {
             return true;
         }
     }
     return false;
 }
 
-void print_maze(char (*maze)[num_cols], Cell *path, int num_steps) {
-    for (int r = 0; r < num_rows; r++) {
+void print_maze(Maze *maze, Path *path) {
+    char (*m)[maze->num_cols] = (char (*)[maze->num_cols])maze->m;
+
+    for (int r = 0; r < maze->num_rows; r++) {
         
         // --- 1. Print the North Walls Row ---
-        for (int c = 0; c < num_cols; c++) {
+        for (int c = 0; c < maze->num_cols; c++) {
             printf("+");
-            if (maze[r][c] & North) {
+            if (m[r][c] & North) {
                 printf("---");
             } else {
                 printf("   ");
@@ -170,23 +193,23 @@ void print_maze(char (*maze)[num_cols], Cell *path, int num_steps) {
         printf("+\n");
 
         // --- 2. Print the Cell Middle Row (West wall, Space/*, East wall) ---
-        for (int c = 0; c < num_cols; c++) {
+        for (int c = 0; c < maze->num_cols; c++) {
             // Print the West wall of the cell
-            if (maze[r][c] & West) {
+            if (m[r][c] & West) {
                 printf("|");
             } else {
                 printf(" ");
             }
 
             // Print the cell interior center (Path marker '*' or empty space)
-            if (is_on_path(r, c, path, num_steps)) {
+            if (is_on_path(r, c, path)) {
                 printf(" * ");
             } else {
                 printf("   ");
             }
         }
         // Caps off the very last East wall of the current row
-        if (maze[r][num_cols - 1] & East) {
+        if (m[r][maze->num_cols - 1] & East) {
             printf("|\n");
         } else {
             printf(" \n");
@@ -195,9 +218,9 @@ void print_maze(char (*maze)[num_cols], Cell *path, int num_steps) {
 
     // --- 3. Print the Final South Wall Row ---
     // The bottom-most outer border needs to be closed out cleanly
-    for (int c = 0; c < num_cols; c++) {
+    for (int c = 0; c < maze->num_cols; c++) {
         printf("+");
-        if (maze[num_rows - 1][c] & South) {
+        if (m[maze->num_rows - 1][c] & South) {
             printf("---");
         } else {
             printf("   ");
@@ -206,7 +229,7 @@ void print_maze(char (*maze)[num_cols], Cell *path, int num_steps) {
     printf("+\n");
 }
 
-Cell* explore(int x, int y, Direction in, char (*maze)[num_cols], int path_idx) {
+Cell* explore(int x, int y, Direction in, Maze *maze, Path *path, int path_idx) {
     Direction dirs[] = {North, East, South, West};
     int num_dirs = sizeof(dirs) / sizeof(dirs[0]);
 
@@ -215,15 +238,15 @@ Cell* explore(int x, int y, Direction in, char (*maze)[num_cols], int path_idx) 
     #endif
 
     // Skip if indices are out of bounds
-    if (x < 0 || x >= num_rows || y < 0 || y >= num_cols) return NULL;
+    if (x < 0 || x >= maze->num_rows || y < 0 || y >= maze->num_cols) return NULL;
 
-    if (x == num_rows-1 && y == num_cols-1) {
+    if (x == maze->num_rows-1 && y == maze->num_cols-1) {
         // We have reached the bottom-right cell: Success!
-        found_path = 1;
-        num_steps = path_idx + 1;
-        path[path_idx].x = x;
-        path[path_idx].y = y;
-        return path + path_idx;
+        path->found_path = 1;
+        path->num_steps = path_idx + 1;
+        path->cells[path_idx].x = x;
+        path->cells[path_idx].y = y;
+        return path->cells + path_idx;
     }
 
     Cell *next = NULL;
@@ -232,45 +255,51 @@ Cell* explore(int x, int y, Direction in, char (*maze)[num_cols], int path_idx) 
         if (dirs[i] == in) continue;
 
         // Bit-wise operation non-zero implies a wall
-        if ((maze[x][y] & dirs[i])) continue;
+        if ((maze->m[x*maze->num_cols + y] & dirs[i])) continue;
 
         switch (dirs[i]) {
             case North:
-                next = explore(x-1, y, South, maze, path_idx+1);
+                next = explore(x-1, y, South, maze, path, path_idx+1);
                 break;
             case East:
-                next = explore(x, y+1, West, maze, path_idx+1);
+                next = explore(x, y+1, West, maze, path, path_idx+1);
                 break;
             case South:
-                next = explore(x+1, y, North, maze, path_idx+1);
+                next = explore(x+1, y, North, maze, path, path_idx+1);
                 break;
             case West:
-                next = explore(x, y-1, East, maze, path_idx+1);
+                next = explore(x, y-1, East, maze, path, path_idx+1);
                 break;
         }
 
         // If found a path, don't search in other directions
-        if (found_path) break;
+        if (path->found_path) break;
     }
 
     if (next) {
         // Found a path
-        path[path_idx].x = x;
-        path[path_idx].y = y;
-        return path + path_idx;
+        path->cells[path_idx].x = x;
+        path->cells[path_idx].y = y;
+        return path->cells + path_idx;
     }
 
     return NULL;
 }
 
-void solve_maze(int num_rows, int num_cols, char (*maze)[num_cols]) {
-    explore(0, 0, West, maze, 0);
-    printf("Found maze path of %d steps: ", num_steps);
-    for (int i = 0; i < num_steps; i++) {
-        printf("(%d, %d) ", path[i].x, path[i].y);
+void solve_maze(Maze *maze) {
+    Path path;
+    path.found_path = false;
+    path.cells = malloc(maze->num_rows * maze->num_cols * sizeof(Cell));
+
+    explore(0, 0, West, maze, &path, 0);
+    printf("Found maze path of %d steps: ", path.num_steps);
+    for (int i = 0; i < path.num_steps; i++) {
+        printf("(%d, %d) ", path.cells[i].x, path.cells[i].y);
     }
     printf("\n\n");
-    print_maze(maze, path, num_steps);
+    print_maze(maze, &path);
+
+    free(path.cells);
 }
 
 
@@ -279,15 +308,16 @@ Main processing
 ----------------------------------------*/
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Error: expecting exactly 1 program argument, the maze filename to process. Quitting...\n");
+        printf("Error: expecting the maze filename to process. Quitting...\n");
         return 1;
     }
 
-    char (*maze)[];
+    Maze maze;
+    read_maze_from_file(&maze, argv[1]);
+    encode_maze(&maze);
     
-    maze = read_maze(argv[1], &num_rows, &num_cols);
-    
-    solve_maze(num_rows, num_cols, maze);
+    solve_maze(&maze);
+    free_maze(&maze);
 
     return 0;
 }
